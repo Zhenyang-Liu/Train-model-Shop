5806450400f782ef14b21b851811ca11191053a7
Joe Paton <joantpat@gmail.com>
2023-11-23 02:48:58 +0000

    Added more filter options

--- src/main/java/helper/Filter.java
+++ src/main/java/helper/Filter.java
@@ -1,10 +1,19 @@
 package helper;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+import model.Brand;
+
 public class Filter {
     public class SortBy{
-        public SortBy(String name, String dbHandle){
+        public SortBy(String name, String dbHandle, boolean asc){
             this.name = name;
             this.dbHandle = dbHandle;
+            this.isAscending = asc;
+        }
+        public SortBy(String name, String dbHandle){
+            this(name, dbHandle, true);
         }
 
         @Override
@@ -18,8 +27,13 @@ public class Filter {
             return this.dbHandle;
         }
 
+        public boolean isAscending(){
+            return this.isAscending;
+        }
+
         private String name;
         private String dbHandle;
+        private boolean isAscending;
     };
     public class PriceRange{
         public PriceRange(float low, float high, String displayName){
@@ -35,7 +49,7 @@ public class Filter {
         @Override
         public String toString()
         {
-            return displayName;
+            return new String(displayName.getBytes(), StandardCharsets.UTF_8);
         }
 
         public float getMin()
@@ -50,6 +64,41 @@ public class Filter {
 
         private float minVal;
         private float maxVal;
+        private String displayName;
+    }
+    public class BrandFilter{
+        public BrandFilter(Brand b, String displayName){
+            this.enclosedBrand = b;
+            this.displayName = displayName;
+        }
+        public BrandFilter(Brand b){
+            this(b, b.getBrandName());
+        }
+        @Override
+        public String toString(){
+            return displayName;
+        }
+        public int getBrand(){
+            return enclosedBrand != null ? enclosedBrand.getBrandID() : -1;
+        }
+        private Brand enclosedBrand;
+        private String displayName;
+    }
+    public class TypeFilter{
+        public TypeFilter(String dbColumn, String displayName){
+            this.dbHandle = dbColumn;
+            this.displayName = displayName;
+        }
+
+        public String toString(){
+            return displayName;
+        }
+
+        public String getDbTable(){
+            return this.dbHandle;
+        }
+
+        private String dbHandle;
         private String displayName;
     }
 }
