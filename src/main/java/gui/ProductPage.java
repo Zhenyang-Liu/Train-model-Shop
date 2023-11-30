396fb7a8be8b0e6ac91d42389b7e3800d882b76d
Joe Paton <joantpat@gmail.com>
2023-11-30 02:32:20 +0000

    Added packwatch

--- src/main/java/gui/ProductPage.java
+++ src/main/java/gui/ProductPage.java
@@ -100,11 +100,11 @@ public class ProductPage extends JFrame {
         initComponents();
 
         setTitle((isStaff ? "Editing " : "Viewing ") + p.getProductName());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(new Dimension(700, 500));
         setResizable(true);
         setLocationRelativeTo(null);
         setVisible(true);
+        pack();
     }    
 
     private JPanel createDescription() {
@@ -554,8 +554,8 @@ public class ProductPage extends JFrame {
     }
     
     public static void main(String[] args){
-        UserSession.getInstance().setCurrentUser(UserDAO.findUserByEmail("manager@manager.com"));
         // UserSession.getInstance().setCurrentUser(UserDAO.findUserByEmail("testemail@gmail.com"));
+        UserSession.getInstance().setCurrentUser(UserDAO.findUserByEmail("testey@gmail.com"));
         ProductPage p = new ProductPage(ProductDAO.getAllProduct().get(6));
         p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         p.setVisible(true);
