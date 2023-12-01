9211349310619e41deb2e3853abcad76d3da3045
Julian <julianjones663@gmail.com>
2023-12-01 06:37:17 +0000

    cleaned up imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,8 @@
 
-import gui.LoginPage;
-import gui.MainPage;
 import gui.WelcomePage;
 import helper.Logging;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import DAO.DatabaseConnectionHandler;
 
 public class Main {
     public static void main(String[] args) {
